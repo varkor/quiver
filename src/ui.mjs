799f943842060b9b17ce17c94282a6f41c09f5dd
varkor <github@varkor.com>
2025-09-20 19:07:04 +0300

    Fix bug with panel not being dismissed

--- src/ui.mjs
+++ src/ui.mjs
@@ -1581,6 +1581,7 @@ class UI {
                                 this.selection_excluding(created),
                             );
                         }
+                        this.panel.hide_if_unselected(this);
                         this.switch_mode(UIMode.default);
                     }
                 }
